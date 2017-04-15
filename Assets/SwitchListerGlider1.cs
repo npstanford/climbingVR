9a185f938b3e004697c86e2b69eebac71eeabe7d
npstanford <npstanford@gmail.com>
2017-04-15 01:57:36 -0700

    Full loop level

---
+++ Assets/SwitchListerGlider1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchListerGlider1 : MonoBehaviour {
+
+
+    public SwitchControl sc;
+
+    public ScriptedMovement sm;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sc.activated)
+        {
+            Activate();
+        }
+    }
+
+    void Activate()
+    {
+        sm.EnableMovement = true;
+    }
+}
